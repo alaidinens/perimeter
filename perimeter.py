6ce9c4272490b0e1d2f7bcf982661bf216bd2623
alaidinens <121866743+alaidinens@users.noreply.github.com>
2023-03-02 22:34:30 +0200

    Add files via upload

---
+++ perimeter.py
@@ -0,0 +1,58 @@
+def dmain():
+
+    def __init__(self, s1, s2, s3):
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+
+s1= float(input("First side: "))
+s2= float(input("Second side: "))
+s3= float(input("Third side: "))
+
+def triangle(self):
+    return self.s1 + self.s2 + self.s3
+def rectangle(self):
+    if self.s1 == " ":
+        return (self.s2 + self.s3) *2
+    if self.s2 == " ":
+        return (self.s1 + self.s3) *2
+    if self.s3 == " ":
+        return(self.s1 + self.s2) * 2
+print("The rectangle's perimeter is: " + rectangle)
+def square(self):
+    if self.s1 and self.s2 == " ":
+        return self.s3 * 4
+    if self.s2  and self.s3== " ":
+        return self.s1 * 4
+    if self.s3 and self.s3 == " ":
+        return self.s2 * 4
+print("The square's perimeter is: " + square)
+
+    
+        
+
+#class userInput(self, s1, s2, s3):
+#
+#    s1= float(input("First side: "))
+#    s2= float(input("Second side: "))
+#    s3= float(input("Third side: "))
+#        
+#    class triangle(userInput):
+#        def perimeterSquare(self, s1):
+#            return (s1**2)
+#        print("The triangle's perimeter is: }" + triangle)
+#    
+#    class square(userInput):
+#        def perimeterSquare(self, s1):
+#            return (s1**2)
+#    
+#    
+#    print("The square's perimeter is: " + square)
+#    
+#    
+#    class rectangle(userInput):
+#        def perimeterRectangle(self, s1, s2):
+#            return ((s1+s2)*2)
+#    
+#    print("The rectangle's perimeter is: " + rectangle)
+#
